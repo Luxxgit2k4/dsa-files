4c4194390d7cf8cb0f544abfa2894d2289aaac30
Lakshmanan P <122290398+Luxxgit2k4@users.noreply.github.com>
2024-11-02 14:11:56 +0530

    Update Pattern1.java

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -1,1 +1,16 @@
-
+/* Program for printing this pattern
+****
+****
+****
+****
+*/
+class Pattern1.java {
+    public static void main(String[] args) {
+        for (int i=0; i<=4; i++) {
+            for (int j=1; j<=4; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
