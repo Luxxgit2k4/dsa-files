f46f1958a6e5c77d4aca4c9ee0fb170ef7accfd1
Lakshmanan P <122290398+Luxxgit2k4@users.noreply.github.com>
2024-11-02 19:33:44 +0530

    Create Pattern8.java

---
+++ Patterns/Pattern8.java
@@ -0,0 +1,26 @@
+/* Program for printing this pattern 
+*********
+ ******* 
+  *****  
+   ***   
+    *
+  */
+class Main {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i =0; i<5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0;k<2*n-(2*i+1); k++){
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
